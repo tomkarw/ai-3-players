5bc3f7bf1931760d2456822377c504b3ad52a4a9
Tomek Karwowski <to.karwowski@gmail.com>
2021-07-06 21:12:58 +0200

    [FEAT] place method

--- src/board.rs
+++ src/board.rs
@@ -1,7 +1,8 @@
+use itertools::iproduct;
 use std::fmt::{Display, Formatter, Result};
 use termion::color;
 
-// const EMPTY_TILE: u8 = 0;
+const EMPTY_TILE: u8 = 0;
 const BG_GREEN: color::Bg<color::Green> = color::Bg(color::Green);
 const BG_BLACK: color::Bg<color::Black> = color::Bg(color::Black);
 const BG_WHITE: color::Bg<color::White> = color::Bg(color::White);
@@ -12,7 +13,7 @@ const RST_CLR: color::Bg<color::Reset> = color::Bg(color::Reset);
 pub(crate) struct BoardState {
     rows: usize,
     columns: usize,
-    board: Vec<i8>,
+    board: Vec<u8>,
 }
 
 impl Display for BoardState {
@@ -68,4 +69,65 @@ impl BoardState {
             board: vec![0; rows * columns],
         }
     }
+
+    /// Create starting position for 3 player game
+    pub(crate) fn setup_three_players(&mut self) {
+        self.board[3 * self.columns + 3] = 1;
+        self.board[3 * self.columns + 5] = 1;
+        self.board[4 * self.columns + 4] = 1;
+        self.board[3 * self.columns + 4] = 2;
+        self.board[5 * self.columns + 3] = 2;
+        self.board[5 * self.columns + 5] = 2;
+        self.board[4 * self.columns + 3] = 3;
+        self.board[4 * self.columns + 5] = 3;
+        self.board[5 * self.columns + 4] = 3;
+    }
+
+    pub(crate) fn place(&mut self, row: usize, column: usize, player: u8) {
+        for (row, column) in self.would_flip(row, column, player) {
+            self.board[row * self.columns + column] = player;
+        }
+        self.board[row * self.columns + column] = player;
+    }
+
+    fn would_flip(&self, row: usize, column: usize, player: u8) -> Vec<(usize, usize)> {
+        let row = row as i32;
+        let column = column as i32;
+        let mut flips = Vec::new();
+        for (dy, dx) in iproduct!(0..3, 0..3) {
+            let dy: i32 = dy - 1;
+            let dx: i32 = dx - 1;
+            let mut visited_discs = Vec::new();
+            if dy == 0 && dx == 0 {
+                continue;
+            }
+            let mut current_row = row + dy;
+            let mut current_column = column + dx;
+            // stop when we reach board border
+            while !(current_row < 0
+                || current_row >= self.rows as i32
+                || current_column < 0
+                || current_column >= self.columns as i32)
+            {
+                match self.board[current_row as usize * self.columns + current_column as usize] {
+                    // we found outflanking disc, append visited disc to flip list
+                    x if x == player => {
+                        flips.append(&mut visited_discs);
+                        break;
+                    }
+                    // reached blank space, no discs could be outflanked
+                    x if x == EMPTY_TILE => break,
+                    // add met disc to visited discs
+                    _ => visited_discs.push((current_row as usize, current_column as usize)),
+                }
+                current_row += dy;
+                current_column += dx;
+            }
+        }
+        flips
+    }
+
+    // fn validate_placing(&self, row: usize, column: usize, player: u8) {
+    //     // TODO
+    // }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,16 @@
 mod board;
 use board::BoardState;
-use termion::color;
+use std::io;
 
 fn main() {
-    let board = BoardState::new(9, 9);
+    let mut board = BoardState::new(9, 9);
+    board.setup_three_players();
+    println!("{}", board);
+    let mut input = String::new();
+    io::stdin()
+        .read_line(&mut input)
+        .expect("error getting stdin");
+    board.place(2, 4, 1);
+    print!("{}", termion::clear::All);
     println!("{}", board);
 }
